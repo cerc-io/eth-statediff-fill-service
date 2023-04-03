190c3bee0718d581bf50ec517ecaef5c109317e6
Roy Crihfield <roy@manteia.ltd>
2023-04-03 15:17:40 +0800

    [dev] test scriptc

--- scripts/run_unit_test.sh
+++ scripts/run_unit_test.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-set -e
+set -ex
 
-# Clear up existing docker images and volume.
-docker-compose down --remove-orphans --volumes
+# # Clear up existing docker images and volume.
+# docker-compose down --remove-orphans --volumes
 
-# Spin up TimescaleDB
-docker-compose -f docker-compose.yml up -d migrations ipld-eth-db
-# sleep 45
-sleep 10
+# # Spin up TimescaleDB
+# docker-compose -f docker-compose.yml up -d migrations ipld-eth-db
+# # sleep 45
+# sleep 10
 
 # Run unit tests
-go clean -testcache
-PGPASSWORD=password DATABASE_USER=vdbm DATABASE_PORT=8077 DATABASE_PASSWORD=password DATABASE_HOSTNAME=127.0.0.1 DATABASE_NAME=vulcanize_testing make test
+# go clean -testcache
+PGPASSWORD=password DATABASE_USER=vdbm DATABASE_PORT=8099 DATABASE_PASSWORD=password DATABASE_HOSTNAME=localhost DATABASE_NAME=vulcanize_testing exec "$@"
 
-# Clean up
-docker-compose down --remove-orphans --volumes
+# # Clean up
+# docker-compose down --remove-orphans --volumes
